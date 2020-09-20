8214f34a850bfcb76209a5b1677ebe9a91225324
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-20 11:38:16 +0100

    added test and method for add guest to room

--- src/room.py
+++ src/room.py
@@ -1,11 +1,11 @@
 class Room:
     def __init__(self, room_name, guest_list, song_list, cash_register):
-        self.room_name = room_name
+        self.room = room_name
         self.guest_list = []
         self.song_list = []
         self.cash_register = cash_register
 
 
-    def add_guest_to_room(self):
-        guest = guest_1
-        self.room.guest_list.append(self.guest_1)
+    def add_guest_to_room(self, guest):
+        # guest = self.room.guest_1
+        self.guest_list.append(guest)
