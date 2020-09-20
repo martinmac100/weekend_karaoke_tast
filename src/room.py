9950fb15b9c40ade7e5168adf360419617b09409
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-20 12:58:43 +0100

    added test add song to list

--- src/room.py
+++ src/room.py
@@ -1,11 +1,13 @@
 class Room:
     def __init__(self, room_name, guest_list, song_list, cash_register):
-        self.room = room_name
+        self.room_name = room_name
         self.guest_list = []
         self.song_list = []
         self.cash_register = cash_register
 
 
     def add_guest_to_room(self, guest):
-        # guest = self.room.guest_1
         self.guest_list.append(guest)
+    
+    def add_song_title_to_room(self, song):
+        self.song_list.append(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -20,8 +20,8 @@ class TestRoom(unittest.TestCase):
         self.guest_3 = Guest("George", 45, 10)
         self.guest_4 = Guest("Ringo", 17, 30)
 
-    # def test_room_has_name(self):
-    #     self.assertEqual("Voodoo", self.room_name)
+    def test_room_has_name(self):
+        self.assertEqual("Voodoo", self.room.room_name)
 
     def test_room_has_guest_list(self):
         self.assertEqual([], self.room.guest_list)
@@ -32,3 +32,8 @@ class TestRoom(unittest.TestCase):
     def test_add_guest_to_room(self):
         self.room.add_guest_to_room(self.guest_1)
         self.assertEqual([self.guest_1],self.room.guest_list)
+
+    def test_add_song_title_to_room(self):
+        self.room.add_song_title_to_room(self.song_1)
+        self.assertEqual([self.song_1],self.room.song_list)
+
